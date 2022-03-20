fc92fee9a07d91ca94fc0e66aecb04f9b7c66cd5
dedencdt <santr.iawam11@gmail.com>
2022-03-20 09:21:53 +0700

    Menambahkan file XML Versi 0

--- application/views/member/market/market_prelanders_xml.php
+++ application/views/member/market/market_prelanders_xml.php
@@ -5,6 +5,7 @@
 <meta property="og:title" content="The Rock" />
 <meta property="og:url" content="https://www.imdb.com/title/tt0117500/" />
 <meta property="og:image" content="https://ia.media-imdb.com/images/rock.jpg" />
+
   <b:skin><![CDATA[
                   
                   /****CSS CODE*****/
