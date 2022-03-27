a572103654c508a5d5ebf0d61b3b8ab35167d498
dedencdt <santr.iawam11@gmail.com>
2022-03-27 18:44:55 +0700

    Test Mailer in registration

--- application/helpers/mail_helper.php
+++ application/helpers/mail_helper.php
@@ -1,7 +1,7 @@
 <?php
 
 
-function send_mail($oThis, $data)
+function send_mail($oThis, $to, $subjet, $msg, $attach = null)
 {
   // Konfigurasi email 
   $config = [
@@ -9,8 +9,8 @@ function send_mail($oThis, $data)
     'charset'   => 'utf-8',
     'protocol'  => 'smtp',
     'smtp_host' => 'smtp.gmail.com',
-    'smtp_user' => 'email@gmail.com',  // Email gmail
-    'smtp_pass'   => 'passwordgmail',  // Password gmail
+    'smtp_user' => 'codtech.info@gmail.com',  // Email gmail
+    'smtp_pass'   => 'wordpress2020',  // Password gmail
     'smtp_crypto' => 'ssl',
     'smtp_port'   => 465,
     'crlf'    => "\r\n",
@@ -21,19 +21,21 @@ function send_mail($oThis, $data)
   $oThis->load->library('email', $config);
 
   // Email dan nama pengirim
-  $oThis->email->from('no-reply@masrud.com', 'MasRud.com');
+  $oThis->email->from('admin@codtech.id', 'Codtech.id');
 
   // Email penerima
-  $oThis->email->to('penerima@domain.com'); // Ganti dengan email tujuan
+  $oThis->email->to($to); // Ganti dengan email tujuan
 
-  // Lampiran email, isi dengan url/path file
-  $oThis->email->attach('https://images.pexels.com/photos/169573/pexels-photo-169573.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940');
+  if ($attach != null) {
+    // Lampiran email, isi dengan url/path file
+    $oThis->email->attach($attach);
+  }
 
   // Subject email
-  $oThis->email->subject('Kirim Email dengan SMTP Gmail CodeIgniter | MasRud.com');
+  $oThis->email->subject($subjet);
 
   // Isi email
-  $oThis->email->message("Ini adalah contoh email yang dikirim menggunakan SMTP Gmail pada CodeIgniter.<br><br> Klik <strong><a href='https://masrud.com/kirim-email-codeigniter/' target='_blank' rel='noopener'>disini</a></strong> untuk melihat tutorialnya.");
+  $oThis->email->message($msg);
 
   // Tampilkan pesan sukses atau error
   if ($oThis->email->send()) {
@@ -42,3 +44,45 @@ function send_mail($oThis, $data)
     echo 'Error! email tidak dapat dikirim.';
   }
 }
+
+// function send_mail($oThis, $data)
+// {
+//   // Konfigurasi email 
+//   $config = [
+//     'mailtype'  => 'html',
+//     'charset'   => 'utf-8',
+//     'protocol'  => 'smtp',
+//     'smtp_host' => 'smtp.gmail.com',
+//     'smtp_user' => 'codtech.info@gmail.com',  // Email gmail
+//     'smtp_pass'   => 'wordpress2020',  // Password gmail
+//     'smtp_crypto' => 'ssl',
+//     'smtp_port'   => 465,
+//     'crlf'    => "\r\n",
+//     'newline' => "\r\n"
+//   ];
+
+//   // Load library email dan konfigurasinya
+//   $oThis->load->library('email', $config);
+
+//   // Email dan nama pengirim
+//   $oThis->email->from('no-reply@masrud.com', 'MasRud.com');
+
+//   // Email penerima
+//   $oThis->email->to('penerima@domain.com'); // Ganti dengan email tujuan
+
+//   // Lampiran email, isi dengan url/path file
+//   $oThis->email->attach('https://images.pexels.com/photos/169573/pexels-photo-169573.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940');
+
+//   // Subject email
+//   $oThis->email->subject('Kirim Email dengan SMTP Gmail CodeIgniter | MasRud.com');
+
+//   // Isi email
+//   $oThis->email->message("Ini adalah contoh email yang dikirim menggunakan SMTP Gmail pada CodeIgniter.<br><br> Klik <strong><a href='https://masrud.com/kirim-email-codeigniter/' target='_blank' rel='noopener'>disini</a></strong> untuk melihat tutorialnya.");
+
+//   // Tampilkan pesan sukses atau error
+//   if ($oThis->email->send()) {
+//     echo 'Sukses! email berhasil dikirim.';
+//   } else {
+//     echo 'Error! email tidak dapat dikirim.';
+//   }
+// }
